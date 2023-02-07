57b506b0e02a8a65e4de0e048c82e1644313e277
Cosimo Bressi <cbressijr@msn.com>
2023-02-07 21:16:52 +0000

    nums

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -28,10 +28,10 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        String str = " ";
-        for(int i = 0; i < nums.length; i++){
-          str = str + String.valueOf(i);
-        }
-        return str;
+      String str = " ";
+      for(int i = 0; i < nums.length; i++){
+        str = str + String.valueOf(i);
       }
+      return str;
+    }
 }
